8d336a5f54599c6ec12412913f5616949fa99297
eduardovsousa <eduardo.varela@outlook.com.br>
2022-08-23 12:03:01 -0300

    Formulário com conexão ao back end

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,47 @@ import Head from 'next/head';
 import HeaderGenric from '../src/components/common/headerGenric';
 import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import Footer from '../src/components/common/footer';
+import { FormEvent, useState } from 'react';
+import authService from '../src/services/authService';
+import { useRouter } from 'next/router';
+import ToastComponent from '../src/components/common/toast';
 
 const Register = function () {
+
+    const router = useRouter();
+    const [toastIsOpen, setToastIsOpen] = useState(false);
+    const [toastMessage, setToastMessage] = useState("");
+    const handleRegister = async (event: FormEvent<HTMLFormElement> ) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget)
+        const firstName = formData.get("firstName")!.toString();
+        const lastName = formData.get("lastName")!.toString();
+        const phone = formData.get("phone")!.toString();
+        const birth = formData.get("birth")!.toString();
+        const email = formData.get("email")!.toString();
+        const password = formData.get("password")!.toString();
+        const confirmPassword = formData.get("confirmPassword")!.toString();
+        const params = {firstName, lastName, phone, birth, email, password };
+
+        if (password != confirmPassword) {
+            setToastIsOpen(true);
+            setTimeout(() => { setToastIsOpen(false) }, 1000 * 3);
+            setToastMessage("Senha e confirmação diferentes!");
+            return;
+        }
+
+        const { data, status } = await authService.register(params)
+
+        if (status === 201) {
+            router.push("/login?registred=true")
+        } else {
+            setToastIsOpen(true);
+            setTimeout(() => { setToastIsOpen(false) }, 1000 * 3);
+            setToastMessage(data.message);
+        }
+    };
+
     return (<>
         <Head>
             <title>Onebitflix - Registro</title>
@@ -16,7 +55,7 @@ const Register = function () {
             <HeaderGenric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login" />
             <Container className="py-5">
                 <p className={ styles.formTitle }><strong>Bem-vindo(a) ao OneBitFlix!</strong></p>
-                <Form className={ styles.form }>
+                <Form className={ styles.form } onSubmit={ handleRegister }>
                     <p className="text-center"><strong>Faça a sua conta!</strong></p>
                     <FormGroup>
                         <Label for="firstName" className={ styles.label }>NOME</Label>
@@ -32,7 +71,7 @@ const Register = function () {
                     </FormGroup>
                     <FormGroup>
                         <Label for="email" className={ styles.label }>E-MAIL</Label>
-                        <Input id="email" name="email" type="email" placeholder="Digite o seu e-mail" required maxLength={20} className={ styles.input }/>
+                        <Input id="email" name="email" type="email" placeholder="Digite o seu e-mail" required className={ styles.input }/>
                     </FormGroup>
                     <FormGroup>
                         <Label for="birth" className={ styles.label }>DATA DE NASCIMENTO</Label>
@@ -43,13 +82,14 @@ const Register = function () {
                         <Input id="password" name="password" type="password" placeholder="Digite a sua senha (Min: 6 | Max: 20)" required minLength={6} maxLength={20} className={ styles.input }/>
                     </FormGroup>
                     <FormGroup>
-                        <Label for="password" className={ styles.label }>CONFIRME SUA SENHA</Label>
-                        <Input id="password" name="password" type="password" placeholder="Confirme a sua senha" required minLength={6} maxLength={20} className={ styles.input }/>
+                        <Label for="confirmPassword" className={ styles.label }>CONFIRME SUA SENHA</Label>
+                        <Input id="confirmPassword" name="confirmPassword" type="password" placeholder="Confirme a sua senha" required minLength={6} maxLength={20} className={ styles.input }/>
                     </FormGroup>
                     <Button type="submit" outline className={ styles.formBtn }>CADASTRAR</Button>
                 </Form>
             </Container>
             <Footer />
+            <ToastComponent color="bg-danger" isOpen={ toastIsOpen } message={ toastMessage }/>
         </main>
     </>)
 }
