70b3addeeecd87ace85bff7aa72cc9fd048817a9
eduardovsousa <eduardo.varela@outlook.com.br>
2022-08-24 16:26:18 -0300

    Criação do header autenticado/logado

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,6 +15,11 @@ const Login = function () {
     const [toastIsOpen, setToastIsOpen] = useState(false);
     const [toastMessage, setToastMessage] = useState("");
 
+    useEffect(() => {
+        if (sessionStorage.getItem("onebitflix-token")) {
+            router.push("/home")
+        }
+    }, [])
     
     useEffect(() => {
         const registerSuccess = router.query.registred;
