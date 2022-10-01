9301576827afee02aee80d88449b05116c20fd12
eduardovsousa <eduardo.varela@outlook.com.br>
2022-10-01 01:36:30 -0300

    Criação da página do curso e métodos no backend para comunicação com o front

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -70,6 +70,28 @@ const courseService = {
         return res;
     },
 
+    addLike: async (courseId: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token")
+
+        const res = await api.post("/likes", { courseId }, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).catch((error) => {
+            return error.response;
+        })
+        return res;
+    },
+
+    removeLike: async (courseId: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token")
+
+        const res = await api.delete(`/likes/${ courseId }`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).catch((error) => {
+            return error.response;
+        })
+        return res;
+    },
+
     getSearch: async (name: string) => {
         const token = sessionStorage.getItem("onebitflix-token")
 
@@ -79,7 +101,18 @@ const courseService = {
             return error.response;
         })
         return res;
-    }
+    },
+
+    getEpisodes: async (id: number | string) => {
+        const token = sessionStorage.getItem("onebitflix-token")
+
+        const res = await api.get(`/courses/${id}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).catch((error) => {
+            return error.response;
+        })
+        return res;
+    },
 };
 
 export default courseService;
